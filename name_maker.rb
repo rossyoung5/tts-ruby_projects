bb883ccb7ec2c3a926ef9bb8f5d6521da46bc2ab
Ross Young <ross.young12@gmail.com>
2016-01-22 13:49:22 -0600

    Added middle name question and full name display

--- name_maker.rb
+++ name_maker.rb
@@ -15,6 +15,28 @@ def display_whole_name(whole_name, name_letters)
   puts "Your name is #{name_letters} letters long."
 end
 
+def middle_name_request(whole_name)
+  puts "Okay, #{whole_name} do you have a middle name?"
+  print "Yes or No? > "
+  gets.chomp.downcase
+end
+
+def middle_name_input(first_name, whole_name)
+middle_name_answer = middle_name_request(whole_name)
+  unless middle_name_answer == "yes"
+    abort("Okay #{first_name}, thanks for the input!")
+  end
+  if middle_name_answer == "yes"
+    puts "Great! What is it?"
+    print "Type it here > "
+    gets.chomp
+  end
+end
+
+def display_three_names(three_names)
+  puts "Hi #{three_names}! That's quite a name."
+end
+
 first_name = first_name_input
 
 last_name = last_name_input(first_name)
@@ -24,3 +46,10 @@ whole_name = first_name + " " + last_name
 name_letters = whole_name.length - 1
 
 display_whole_name(whole_name, name_letters)
+
+middle_name = middle_name_input(first_name, whole_name)
+
+three_names = first_name + " " + middle_name + " " + last_name
+
+display_three_names(three_names)
+
